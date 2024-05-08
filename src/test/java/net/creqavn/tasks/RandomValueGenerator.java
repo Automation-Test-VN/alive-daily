872a93b9d3bc7ac3915ad997f385392bdc9c528a
Tan Dat <dat.tran@creqavn.net>
2024-05-08 17:53:00 +0700

    remove folders

---
+++ src/test/java/net/creqavn/tasks/RandomValueGenerator.java
@@ -0,0 +1,36 @@
+package net.creqavn.tasks;
+import java.util.Random;
+
+public class RandomValueGenerator {
+
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private static final String NUMBERS = "0123456789";
+
+    public static String generateRandomString(int length) {
+        StringBuilder sb = new StringBuilder(length);
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            sb.append(CHARACTERS.charAt(random.nextInt(CHARACTERS.length())));
+        }
+        return sb.toString();
+    }
+
+    public static String generateRandomNumericString(int length) {
+        StringBuilder sb = new StringBuilder(length);
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            sb.append(NUMBERS.charAt(random.nextInt(NUMBERS.length())));
+        }
+        return sb.toString();
+    }
+
+    public static String generateRandomPhoneNumber() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("09");
+        Random random = new Random();
+        for (int i = 0; i < 9; i++) {
+            sb.append(NUMBERS.charAt(random.nextInt(NUMBERS.length())));
+        }
+        return sb.toString();
+    }
+}
