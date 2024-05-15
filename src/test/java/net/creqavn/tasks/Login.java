2ebe9dff5ac9b0bcddbe4abbb38d31589d376856
Tan Dat <dat.tran@creqavn.net>
2024-05-15 13:34:23 +0700

    perf: add code check login status before perform login account

--- src/test/java/net/creqavn/tasks/Login.java
+++ src/test/java/net/creqavn/tasks/Login.java
@@ -1,6 +1,7 @@
 package net.creqavn.tasks;
 
 import net.creqavn.models.LoginAccount;
+import net.creqavn.questions.TheLoginState;
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
@@ -10,37 +11,49 @@ import static net.creqavn.ui.mayman.Lucky88Elements.*;
 
 public class Login {
     public static Performable theAccountHasBalance(LoginAccount loginAccount) {
-        return Task.where("{0} login the account", actor -> {
-            actor.attemptsTo(
-                    Enter.keyValues(ACCOUNT_HAS_BALANCE).into(LOGIN_USERNAME),
-                    Enter.keyValues(loginAccount.getPwd()).into(LOGIN_PWD),
-                    Click.on(LOGIN_SUBMIT)
-            );
-        });
+        return Task.where("{0} login the account has balance", actor -> {
+                    boolean loginState = TheLoginState.isElementPresent(AVATAR_USER).answeredBy(actor);
+                    if (!loginState) {
+                        actor.attemptsTo(
+                                Enter.keyValues(ACCOUNT_HAS_BALANCE).into(LOGIN_USERNAME),
+                                Enter.keyValues(loginAccount.getPwd()).into(LOGIN_PWD),
+                                Click.on(LOGIN_SUBMIT)
+                        );
+                    }
+                }
+        );
     }
 
     public static Performable theAccountNonBalance(LoginAccount loginAccount) {
-        return Task.where("{0} login the account", actor -> {
-            actor.attemptsTo(
-                    Enter.keyValues(ACCOUNT_NON_BALANCE).into(LOGIN_USERNAME),
-                    Enter.keyValues(loginAccount.getPwd()).into(LOGIN_PWD),
-                    Click.on(LOGIN_SUBMIT)
-            );
-        });
+        return Task.where("{0} login the account non balance", actor -> {
+                    boolean loginState = TheLoginState.isElementPresent(AVATAR_USER).answeredBy(actor);
+                    if (!loginState) {
+                        actor.attemptsTo(
+                                Enter.keyValues(ACCOUNT_NON_BALANCE).into(LOGIN_USERNAME),
+                                Enter.keyValues(loginAccount.getPwd()).into(LOGIN_PWD),
+                                Click.on(LOGIN_SUBMIT)
+                        );
+                    }
+                }
+        );
     }
 
     public static Performable theAccountJustRegistered(LoginAccount loginAccount) {
-        return Task.where("{0} login the account", actor -> {
-            actor.attemptsTo(
-                    Enter.keyValues(loginAccount.getUserName()).into(LOGIN_USERNAME),
-                    Enter.keyValues(loginAccount.getPwd()).into(LOGIN_PWD),
-                    Click.on(LOGIN_SUBMIT)
-            );
-        });
+        return Task.where("{0} login the account using account registered", actor -> {
+                    boolean loginState = TheLoginState.isElementPresent(AVATAR_USER).answeredBy(actor);
+                    if (!loginState) {
+                        actor.attemptsTo(
+                                Enter.keyValues(loginAccount.getUserName()).into(LOGIN_USERNAME),
+                                Enter.keyValues(loginAccount.getPwd()).into(LOGIN_PWD),
+                                Click.on(LOGIN_SUBMIT)
+                        );
+                    }
+                }
+        );
     }
 
     public static Performable theAccountHasBalanceOnPopup(LoginAccount loginAccount) {
-        return Task.where("{0} login the account", actor -> {
+        return Task.where("{0} login the account has balance on login popup", actor -> {
             actor.attemptsTo(
                     Enter.keyValues(ACCOUNT_HAS_BALANCE).into(FORM_LOGIN_USERNAME),
                     Enter.keyValues(loginAccount.getPwd()).into(FORM_LOGIN_PWD),
@@ -50,7 +63,7 @@ public class Login {
     }
 
     public static Performable theAccountNonBalanceOnPopup(LoginAccount loginAccount) {
-        return Task.where("{0} login the account", actor -> {
+        return Task.where("{0} login the account non balance on login popup", actor -> {
             actor.attemptsTo(
                     Enter.keyValues(ACCOUNT_NON_BALANCE).into(FORM_LOGIN_USERNAME),
                     Enter.keyValues(loginAccount.getPwd()).into(FORM_LOGIN_PWD),
