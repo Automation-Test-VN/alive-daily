a74977cf29b34f54f418642ec8391e30e7d1462b
Tan Dat <dat.tran@creqavn.net>
2024-05-27 16:05:15 +0700

    feat: add Access.java

---
+++ src/test/java/net/creqavn/tasks/Access.java
@@ -0,0 +1,40 @@
+package net.creqavn.tasks;
+
+import net.serenitybdd.screenplay.Performable;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.HoverOverElement;
+import net.serenitybdd.screenplay.actions.JavaScriptClick;
+import net.serenitybdd.screenplay.targets.Target;
+
+import static net.creqavn.ui.leo.LeoElements.*;
+
+public class Access {
+    public static Performable theGameFrom(Target functionName, Target subFunction) {
+        return Task.where("{0} access game from function", actor -> {
+            actor.attemptsTo(
+                    HoverOverElement.over(functionName),
+                    Click.on(subFunction),
+                    JavaScriptClick.on(CASINO_INDEX_PLAY_BTN.of("1"))
+            );
+        });
+    }
+
+    public static Performable theGameFrom(Target functionName) {
+        return Task.where("{0} access game from function", actor -> {
+            actor.attemptsTo(
+                    Click.on(functionName),
+                    JavaScriptClick.on(CASINO_INDEX_PLAY_BTN.of("1"))
+            );
+        });
+    }
+
+    public static Performable theSport(Target sportBTN) {
+        return Task.where("{0} access sport ",actor -> {
+            actor.attemptsTo(
+                    HoverOverElement.over(SPORTS_BTN),
+                    Click.on(sportBTN)
+            );
+        });
+    }
+}
